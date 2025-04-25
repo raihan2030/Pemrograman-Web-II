ab7faa7ce094e51091d90ae3725dfa103e141d65
raihan2030 <mr799903@gmail.com>
2025-04-25 19:53:54 +0800

    revisi soal 3

--- Praktikum-4/PRAK403.php
+++ Praktikum-4/PRAK403.php
@@ -18,7 +18,15 @@ $arrayData = [
         "Mata Kuliah" => ["Rekayasa Perangkat Lunak", "Analisis dan Perancangan Sistem", "Komputasi Awan", "Kecerdasan Bisnis"],
         "SKS" => [3, 3, 3, 3],
     ]
-]
+];
+
+function sumSKS($sksArray) {
+    $total = 0;
+    foreach ($sksArray as $sks) {
+        $total += $sks;
+    }
+    return $total;
+}
 
 ?>
 
@@ -28,8 +36,57 @@ $arrayData = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PRAK403</title>
+    <style>
+        th {
+            background-color: lightgray;
+        }
+        th, td {
+            padding: 10px;
+        }
+    </style>
 </head>
 <body>
-    Hello, this is PRAK403!<br>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Mata Kuliah diambil</th>
+                <th>SKS</th>
+                <th>Total SKS</th>
+                <th>Keterangan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($arrayData as $data): ?>
+                <?php for($i = 0; $i < count($data["Mata Kuliah"]); $i++): ?>
+                    <tr>
+                        <?php if ($i == 0): ?>
+                            <td><?php echo $data["No"]; ?></td>
+                            <td><?php echo $data["Nama"]; ?></td>
+                        <?php else: ?>
+                            <td></td>
+                            <td></td>
+                        <?php endif; ?>
+
+                        <td><?php echo $data["Mata Kuliah"][$i]; ?></td>
+                        <td><?php echo $data["SKS"][$i]; ?></td>
+
+                        <?php if ($i == 0): ?>
+                            <td><?php echo sumSKS($data['SKS']) ?></td>
+                            <?php if(sumSKS($data['SKS']) < 7): ?>
+                                <td style="background-color: red;">Revisi KRS</td>
+                            <?php else: ?>
+                                <td style="background-color: lime;">Tidak Revisi</td>
+                            <?php endif; ?>
+                        <?php else: ?>
+                            <td></td>
+                            <td></td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endfor; ?>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
